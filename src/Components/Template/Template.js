6179a39f8aa62f695df69ff2997bdd5e3784b6e2
divyansh351 <divyanshgupta351@gmail.com>
2024-08-10 14:11:11 +0530

    Folder Structure

---
+++ src/Components/Template/Template.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { TextField, Button, Container, Typography, Box } from '@mui/material';
+
+const FormComponent = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form Data:', formData);
+    };
+
+    return (
+        <Container maxWidth="sm">
+            <Box sx={{ mt: 5 }}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Sign Up
+                </Typography>
+                <form onSubmit={handleSubmit}>
+                    <TextField
+                        label="Name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                        required
+                    />
+                    <TextField
+                        label="Email"
+                        name="email"
+                        type="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                        required
+                    />
+                    <TextField
+                        label="Password"
+                        name="password"
+                        type="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        fullWidth
+                        margin="normal"
+                        required
+                    />
+                    <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+                        Submit
+                    </Button>
+                </form>
+            </Box>
+        </Container>
+    );
+};
+
+export default FormComponent;
